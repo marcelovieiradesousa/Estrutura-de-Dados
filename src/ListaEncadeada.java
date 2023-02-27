884313c1ba6d38c61c97e481d0805eaa1db3c064
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-02-27 19:28:57 -0300

    ListaEncadeada: add, isEmpty and size Method

---
+++ src/ListaEncadeada.java
@@ -0,0 +1,42 @@
+public class ListaEncadeada<T> {
+  private No<T> referenciaEntrada;
+
+  public ListaEncadeada() {
+    this.referenciaEntrada = null;
+  }
+
+  public void add(T conteudo) {
+    No<T> novoNo = new No(conteudo);
+    if (this.isEmpty()) {
+      referenciaEntrada = novoNo;
+      return;
+    }
+    No<T> noAuxiliar = referenciaEntrada;
+    for (int i = 0; i < size() - 1; i++) {
+      noAuxiliar = noAuxiliar.getProximoNo();
+    }
+    noAuxiliar.setProximoNo(novoNo);
+  }
+
+  private int size() {
+    int tamanhoLista = 0;
+    No<T> referenciaAux = referenciaEntrada;
+    while (true) {
+      if (referenciaAux != null) {
+        tamanhoLista++;
+        if (referenciaAux.getProximoNo() != null) {
+          referenciaAux = referenciaAux.getProximoNo();
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+    return tamanhoLista;
+  }
+
+  public boolean isEmpty() {
+    return referenciaEntrada == null ? true : false;
+  }
+}
